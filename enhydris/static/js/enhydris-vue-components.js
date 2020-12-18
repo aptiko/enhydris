462b4bb91c1bfa4520a5bc568aa3215456b07769
Antonis Christofides <antonis@antonischristofides.com>
2020-12-18 15:19:35 +0200

    Add vue-i18n

--- enhydris/static/js/enhydris-vue-components.js
+++ enhydris/static/js/enhydris-vue-components.js
@@ -1,10 +1,22 @@
+const messages = {
+  el: {
+    Login: 'Είσοδος',
+  },
+};
+
+const i18n = VueI18n.createI18n({
+  locale: enhydris.activeTranslation,
+  messages,
+});
+
 enhydris.VueApp = {
 };
 
 const app = Vue.createApp(enhydris.VueApp);
+app.use(i18n);
 
 app.component('user-menu-items', {
-  template: '<li><a class="nav-link" href="#">Login</a></li>',
+  template: '<li><a class="nav-link" href="#">{{ $t("Login") }}</a></li>',
 });
 
 app.mount('body');
