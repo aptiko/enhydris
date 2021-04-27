9440ba7c7f4c4f21a49a883a06e6c6ec01fc42a3
Antonis Christofides <antonis@antonischristofides.com>
2021-04-27 21:55:14 +0300

    Downgrade to vue 2 and use bootstrap-vue

--- enhydris/static/js/enhydris-vue-components.js
+++ enhydris/static/js/enhydris-vue-components.js
@@ -9,41 +9,27 @@ const messages = {
   },
 };
 
-const i18n = VueI18n.createI18n({
+const i18n = new VueI18n({
   locale: enhydris.activeTranslation,
   messages,
 });
 
 const vueStore = {
   loggedOnUser: enhydris.loggedOnUser,
-  loginFormVisible: false,
 };
 
-enhydris.VueApp = {
-};
-
-const app = Vue.createApp(enhydris.VueApp);
-app.use(i18n);
-
-app.component('user-menu-items', {
-  data() {
-    return { sharedState: vueStore };
-  },
-  template: '<DashboardButton/><LoginButton/>',
-});
-
-app.component('DashboardButton', {
+Vue.component('DashboardButton', {
   data() {
     return { sharedState: vueStore };
   },
   template: `
-    <li v-if="sharedState.loggedOnUser">
-      <a class="nav-link" href="/admin/">{{ $t("Dashboard") }}</a>
-    </li>
+    <b-nav-item v-if="sharedState.loggedOnUser" href="/admin/">
+      {{ $t("Dashboard") }}
+    </b-nav-item>
   `,
 });
 
-app.component('LoginButton', {
+Vue.component('LoginButton', {
   data() {
     return {
       sharedState: vueStore,
@@ -51,16 +37,14 @@ app.component('LoginButton', {
     };
   },
   template: `
-    <li>
-      <a v-if="sharedState.loggedOnUser" class="nav-link" href="#" v-on:click="logout">
-        <span v-if="loggingOut" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      <b-nav-item v-if="sharedState.loggedOnUser" v-on:click="logout">
+        <b-spinner v-if="loggingOut"></b-spinner>
         {{ $t("Logout") }}
-      </a>
-      <a v-else class="nav-link" href="#" v-on:click="login">
+      </b-nav-item>
+      <b-nav-item v-else v-b-modal.login-form>
         {{ $t("Login") }}
-      </a>
-    </li>
-    <LoginForm/>
+        <LoginForm/>
+      </b-nav-item>
   `,
   methods: {
     logout() {
@@ -72,86 +56,48 @@ app.component('LoginButton', {
         })
         .catch(() => { this.loggingOut = false; });
     },
-    login() {
-      this.sharedState.loginFormVisible = true;
-    },
   },
 });
 
-app.component('LoginForm', {
+Vue.component('LoginForm', {
   data() {
     return {
       sharedState: vueStore,
-      loginModal: null,
       errors: '',
-      loggingIn: false,
+      username: '',
+      password: '',
     };
   },
   template: `
-    <div id="login-form" class="modal" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">{{ $t("Login") }}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" v-on:click="cancel"></button>
-          </div>
-          <div class="modal-body">
-            <p v-if="errors" class="text-danger">{{ errors }}</p>
-            <form>
-              <div class="mb-3">
-                <label for="username" class="form-label">{{ $t("Username") }}</label>
-                <input type="text" class="form-control" id="username">
-              </div>
-              <div class="mb-3">
-                <label for="password" class="form-label">{{ $t("Password") }}</label>
-                <input type="password" class="form-control" id="password">
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" v-on:click="cancel">{{ $t("Cancel") }}</button>
-            <button type="button" class="btn btn-primary" v-on:click="doLogin">
-              <span v-if="loggingIn" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-              {{ $t("Login") }}
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <b-modal id="login-form" :title="$t('Login')" @ok="doLogin" :ok-title="$t('Login')" :cancel-title="$t('Cancel')">
+      <p v-if="errors" class="text-danger">{{ errors }}</p>
+      <b-form-input v-model="username" :placeholder="$t('Username')"></b-form-input>
+      <b-form-input v-model="password" type="password" :placeholder="$t('Password')"></b-form-input>
+    </b-modal>
+   </div>
   `,
-  mounted() {
-    this.loginModal = new bootstrap.Modal(document.querySelector('#login-form'), {});
-  },
-  watch: {
-    'sharedState.loginFormVisible': function () {
-      if (this.sharedState.loginFormVisible) {
-        this.loginModal.show();
-      } else {
-        this.loginModal.hide();
-      }
-    },
-  },
   methods: {
-    cancel() {
-      this.sharedState.loginFormVisible = false;
-    },
-    doLogin() {
+    doLogin(bvModalEvent) {
+      bvModalEvent.preventDefault();
       const that = this;
-      const username = document.querySelector('#username').value;
-      const password = document.querySelector('#password').value;
-      that.loggingIn = true;
-      axios.post('/api/auth/login/', { username, password })
+      axios.post('/api/auth/login/', { username: that.username, password: that.password })
         .then(function () {
-          that.sharedState.loggedOnUser = username;
-          that.loggingIn = false;
-          that.sharedState.loginFormVisible = false;
+          that.sharedState.loggedOnUser = that.username;
+          that.$bvModal.hide('login-form');
         })
         .catch(function (error) {
-          that.loggingIn = false;
           [that.errors] = error.response.data.non_field_errors;
         });
     },
   },
 });
 
-app.mount('body');
+const app = new Vue({
+  i18n,
+  el: 'user-menu-items',
+  template: '<b-navbar-nav class="ml-auto"><DashboardButton/><LoginButton/></b-navbar-nav>',
+  data() {
+    return { sharedState: vueStore };
+  },
+});
